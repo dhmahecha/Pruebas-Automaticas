80a195550635cd5cfb8b7671719a69566501df6c
Unknown <dh.mahecha@uniandes.edu.co>
2018-04-17 01:54:36 -0500

    Ajuste

---
+++ mutode/index.js
@@ -0,0 +1,6 @@
+// This is not the main file in the npm package, but here so we can use github
+// tarballs as packages when necessary.
+module.exports = require("./app/prestashop/js/tools.js");
+module.exports = require("./app/prestashop/js/validate.js");
+module.exports = require("./app/prestashop/js/date.js");
+
